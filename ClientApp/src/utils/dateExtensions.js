7248b1c783544abc51d631b67a12cdab64cdffd0
tothevoid <tothevoid42@gmail.com>
2021-08-13 17:25:35 +0300

    moved date func to utils

--- ClientApp/src/utils/dateExtensions.js
+++ ClientApp/src/utils/dateExtensions.js
@@ -2,6 +2,12 @@ export const formatDate = (date) =>
     addZeroes(date.getDate()) + "/" + addZeroes((date.getMonth() + 1)) + "/" + date.getFullYear() 
         + " " + addZeroes(date.getHours()) + ":" + addZeroes(date.getMinutes());
 
+export const getCurrentDate = () => {
+    const date = new Date();
+    return new Date(date.getTime() - (date.getTimezoneOffset() * 60000))
+        .toISOString().substr(0, 10);
+}
+
 const addZeroes = (value) => 
     (value <= 9) ?
         "0" + value :

a23c0b68c7eb82c7818b5be2df333705ea4bf1cc
tothevoid <tothevoid42@gmail.com>
2021-09-18 16:36:43 +0300

    added db auth and env config

--- ServerApp/Settings/DatabaseSettings.cs
+++ ServerApp/Settings/DatabaseSettings.cs
@@ -8,7 +8,16 @@
 
         public string Port { get; set; }
 
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
         public string ConnectionString =>
-            $@"mongodb://{Host}:{Port}";
+           HasAuth ?
+                $@"mongodb://{Login}:{Password}@{Host}:{Port}" :
+                $@"mongodb://{Host}:{Port}";
+
+        private bool HasAuth =>
+             !string.IsNullOrEmpty(Login) && !string.IsNullOrEmpty(Password);
     }
 }
